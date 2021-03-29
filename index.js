f4447cbf1ae39a32e9f3a308bfc2303254b0f7fa
cade-conklin <69650752+cade-conklin@users.noreply.github.com>
2021-03-28 19:03:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Card extends React.Component {
+    render() {
+      return (
+        <button className="card" >
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {
+    renderCard(i) {
+      return <Card />;
+    }
+
+    createTable = () => {
+        let table = []
+    
+        // Outer loop to create parent
+        for (let i = 0; i < 3; i++) {
+          let children = []
+          //Inner loop to create children
+          for (let j = 0; j < 6; j++) {
+            children.push(this.renderCard(i))
+          }
+          //Create the parent and add the children
+          table.push(<div className="board-row">{children}</div>)
+        }
+        return table
+      }
+
+    render() {
+      const status = 'Basketball Cards';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          {this.createTable()}
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
