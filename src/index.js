b01e34d7538be708ac72269274394a6feb7174e1
cade-conklin <conklincade@gmail.com>
2021-03-30 21:53:01 -0700

    Using API to get names for each card

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Card extends React.Component {
     render() {
       return (
-        <div className="card" onClick={this.myfunction}>
+        <div className="card" onClick={this.clickFunction}>
           <img src="noperson3.png" alt="my image"/>
           <div className="container">
             <h4><b>Damian Lillard</b></h4>
@@ -15,23 +15,77 @@ class Card extends React.Component {
         </div>
       );
     }
-    myfunction() {
-      alert("Clicked");
-      console.log("CLICKED");
-  }
+    clickFunction() {
+      alert('Click');
+    }
   }
   
   class Board extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        first_name: "",
+        last_name: "",
+        doneAPI: false,
+      };
+    }
+
     renderCard(i) {
-      return <Card />;
+      return(
+        <div className="card" onClick={this.clickFunction}>
+          <img src="noperson3.png" alt="my image"/>
+          <div className="container">
+            <h4><b>{this.state.first_name}</b></h4>
+            <p>{this.state.last_name}</p>
+          </div>
+          {/* TODO */}
+        </div>
+      );
+    }
+
+    clickFunction() {
+          alert('Click');
+        }
+
+    myfunction() {
+      fetch('https://www.balldontlie.io/api/v1/players/237')
+        .then(async response => {
+            const data = await response.json();
+            console.log(data.first_name);
+            console.log(data.last_name);
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response statusText
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            this.setState({first_name: data.first_name})
+            this.setState({last_name: data.last_name}) 
+            this.setState({doneAPI: true}) 
+        })
+        .catch(error => {
+            // this.setState({ errorMessage: error.toString() });
+            console.error('There was an error!', error);
+        });
     }
 
     createTable = () => {
         let table = []
+        if (!this.state.doneAPI) {
+          this.myfunction();
+        }
+      //  const first_name = data.parse("first_name");
+      //  const last_name = data.parse("last_name");
+      //  console.log(first_name);
+      //  console.log(last_name);
+       
     
         // Outer loop to create parent
         for (let i = 0; i < 18; i++) {
           let children = []
+          console.log("Here");
             children.push(this.renderCard(i))
           //Create the parent and add the children
           table.push(<div className="board-row">{children}</div>)
